dbaf1e1aca035a486c255b4592387ad9376d3c48
Ryan Wessel <rwessel@dataminr.com>
2022-04-20 17:50:55 -0400

    Add Okta and fetch example to repo

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import {
+    createConfig,
+} from "@okta/okta-react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [data, setData] = useState(null);
+    const [oktaReady, setOktaReady] = useState(false);
+
+    useEffect(() => {
+        const oktaInit = async () => {
+            try {
+                const oktaReady = await createConfig({
+                    clientId: "mockClientId",
+                    redirectUri: `https://fakeauth.com:/login`,
+                    endSessionRedirectUri: `https://fakeauth.com:/logout`,
+                    discoveryUri:
+                        "https://mock-123.okta.com/oauth2/default",
+                    scopes: ["openid", "profile"],
+                });
+                setOktaReady(oktaReady);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        oktaInit();
+    }, []);
+
+    useEffect(() => {
+        fetchMockData();
+    }, []);
+
+    const fetchMockData = () => {
+        fetch("https://625885c892dc8873186269f4.mockapi.io/data")
+            .then((res) => {
+                console.log(res);
+                return res.json();
+            })
+            .then((res) => {
+                setData(res);
+            })
+            .catch((err) => {
+                console.error("request failed", err);
+            });
+    };
+    return (
+        <View style={styles.container}>
+            <Text>{`Okta ready: ${oktaReady}`}</Text>
+            <Text>{JSON.stringify(data?.[0])}</Text>
+            <Button title="Make API Request" onPress={fetchMockData}>
+                Make request
+            </Button>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });

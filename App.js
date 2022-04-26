c4fa0c5355d5cc163bd50384e95b5dc4705fef01
Ryan Wessel <rwessel@dataminr.com>
2022-04-26 11:59:22 -0400

    Add Okta, fetch fails without any other dependencies

--- App.js
+++ App.js
@@ -1,8 +1,29 @@
 import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
+import {createConfig} from '@okta/okta-react-native';
 
 export default function App() {
   const [data, setData] = useState(null);
+  const [oktaReady, setOktaReady] = useState(false);
+
+  useEffect(() => {
+    const oktaInit = async () => {
+      try {
+        const initialized = await createConfig({
+          clientId: 'mockClientId',
+          redirectUri: `https://fakeauth.com:/login`,
+          endSessionRedirectUri: `https://fakeauth.com:/logout`,
+          discoveryUri: 'https://mock-123.okta.com/oauth2/default',
+          scopes: ['openid', 'profile'],
+          requireHardwareBackedKeyStore: false,
+        });
+        setOktaReady(initialized);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    oktaInit();
+  }, []);
 
   useEffect(() => {
     fetchMockData();
@@ -23,6 +44,7 @@ export default function App() {
   };
   return (
     <View style={styles.container}>
+      <Text>{`Okta ready: ${oktaReady}`}</Text>
       <Text>{JSON.stringify(data?.[0])}</Text>
       <Button title="Make API Request" onPress={fetchMockData}>
         Make request
